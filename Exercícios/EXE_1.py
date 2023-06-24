7dd5eda9587d0aacb159931bf973e02790d8648c
Beatriz Mesquita <beatrizmesquita158@gmail.com>
2023-06-24 19:46:43 -0300

    Exercício 1
    Soma dos pontos de 3 jogadores

---
+++ Exercícios/EXE_1.py
@@ -0,0 +1,7 @@
+#Soma dos pontos de 3 jogadores
+
+jog1 = int(input("Jogador número 1 tem: "))
+jog2 = int(input("Jogador número 2 tem: "))
+jog3 = int(input("Jogador número 3 tem: "))
+soma = jog1 + jog2 + jog3
+print(f"A soma dos pontos dos três jogadores: {soma}")
