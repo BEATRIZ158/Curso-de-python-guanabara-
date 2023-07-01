774c22237d8b7366921371b81770ba44b8291454
Beatriz Mesquita <beatrizmesquita158@gmail.com>
2023-07-01 11:33:01 -0300

    Exercício 3

---
+++ Exercícios/EXE_3.py
@@ -0,0 +1,5 @@
+#Programa para ler dois números e mostrar a soma dos valores
+n1 = int(input("Digite um primeiro número: ")) #Variável para armazenar o primeiro número
+n2 = int(input("Digite um segundo número: ")) #Variável para armazenar o segundo número
+soma = n1 + n2 #Variável para armazenar a soma dos valores
+print(f"A soma entre {n1} e {n2}: {soma}") #Mostrar os valores na tela.
