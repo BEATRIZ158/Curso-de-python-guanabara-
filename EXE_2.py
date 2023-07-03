e35206bb8dab9cd0808e0d5bec926cbe528f1c01
Beatriz Mesquita <beatrizmesquita158@gmail.com>
2023-07-02 22:38:39 -0300

    Delete EXE_2.py

--- EXE_2.py
+++
@@ -1,4 +0,0 @@
-#Interação com o usuário
-nome = input("Qual é seu nome: ") #A função INPUT é utilizada para dar entrada no que o usuário digtou, ou seja, "LER" o nome, e armazenar o conteudo na variável NOME.
-print(f"É um prazer conhecer você {nome}!")  #PRINT é uma função para "Imprimir" a mensagem na tela.
-#O f no início das chaves, é uma f-string, é utilizada para substituir a função FORMAT.
