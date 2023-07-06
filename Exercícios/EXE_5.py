1407f09e5f0f3682101a6d90de43c812ff274f57
Beatriz Mesquita <beatrizmesquita158@gmail.com>
2023-07-06 10:14:18 -0300

    Exercício 5

---
+++ Exercícios/EXE_5.py
@@ -0,0 +1,5 @@
+#Mostrar o antecessor e o sucesser de um número inteiro
+num = int(input("Digite um valor inteiro: "))
+ant = num-1 #Variável que recebe a variável num - 1
+suc = num+1 #Variável que recebe a variável num + 1
+print(f"Antecessor: {ant}\nSucessor: {suc}")
